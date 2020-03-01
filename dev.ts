8c7648378700c9585acea107e62e0c1084126e01
George Kampitakis <gkabitakis@gmail.com>
2020-03-01 13:34:58 +0000

    Changes for publishig module

--- dev.ts
+++ dev.ts
@@ -1,121 +1,117 @@
 import { Schema, MongoDriver, Model, Document } from './index';
 
 const userSchema = new Schema({
-	// username: {//This must throw error as well
-	//   required: true,
-	//   type: "string",
-	//   "default": []
-	// }
-	username: {
-		required: true,
-		type: 'string'
-	}
-	// username: {
-	//   type: String,
-	//   required: true
-	// },
-	// email: {
-	//   type: String,
-	//   required: true
-	// },
-	// password: {
-	//   type: String,
-	//   required: true
-	// },
-	// salt: {
-	//   type: String
-	// },
-	// verified: {
-	//   type: Boolean,
-	//   default: false
-	// },
-	// createdAt: {
-	//   type: Date,
-	//   default: Date.now()
-	// },
-	// documents: {
-	//   type: [{ type: ObjectId, ref: JsonDocModel }],
-	//   default: []
-	// }
+  // username: {//This must throw error as well
+  //   required: true,
+  //   type: "string",
+  //   "default": []
+  // }
+  username: {
+    required: true,
+    type: 'string'
+  }
+  // username: {
+  //   type: String,
+  //   required: true
+  // },
+  // email: {
+  //   type: String,
+  //   required: true
+  // },
+  // password: {
+  //   type: String,
+  //   required: true
+  // },
+  // salt: {
+  //   type: String
+  // },
+  // verified: {
+  //   type: Boolean,
+  //   default: false
+  // },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now()
+  // },
+  // documents: {
+  //   type: [{ type: ObjectId, ref: JsonDocModel }],
+  //   default: []
+  // }
 });
 
 MongoDriver.connect('mongodb://localhost:27017', 'mongoDriver', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-	.then(async () => {
-		try {
-			// const userModel = Model('User', userSchema);
-			// userSchema.test();
-			// const userModel1 = Model('User', userSchema);
-			// const userModel2 = Model('User', userSchema);
-			// const userModel3 = Model('User', userSchema);
+  .then(async () => {
+    try {
+      // const userModel = Model('User', userSchema);
+      // userSchema.test();
+      // const userModel1 = Model('User', userSchema);
+      // const userModel2 = Model('User', userSchema);
+      // const userModel3 = Model('User', userSchema);
 
-			// const newUser = await userModel.create({ username: 'George' });
+      // const newUser = await userModel.create({ username: 'George' });
 
-			// console.log(newUser);
+      // console.log(newUser);
 
-			// const test = userModel.instance({ username: 'test' });
-			// console.log(test);
-			// console.log(await test.save());
+      // const test = userModel.instance({ username: 'test' });
+      // console.log(test);
+      // console.log(await test.save());
 
-			// console.log(test.data._id);
+      // console.log(test.data._id);
 
-			//---------------------------------------------
+      //---------------------------------------------
 
-			// console.log(await userModel.findByIdAndUpdate('5e4c914a23ffc7164efd9537', { let: 'test' }, { returnOriginal: false }));
+      // console.log(await userModel.findByIdAndUpdate('5e4c914a23ffc7164efd9537', { let: 'test' }, { returnOriginal: false }));
 
-			// const user = await userModel.create({ username: 'test' });
-			// console.log(user.data.username);
-			// console.log(user.data._id);
+      // const user = await userModel.create({ username: 'test' });
+      // console.log(user.data.username);
+      // console.log(user.data._id);
 
-			// console.log(await userModel1.findById(user.data._id));
+      // console.log(await userModel1.findById(user.data._id));
 
-			// await userModel.create({ blue: 'test' }); //This must return error
+      // await userModel.create({ blue: 'test' }); //This must return error
 
-			//--------------------------------------------
+      //--------------------------------------------
 
-			// const test = userModel.instance({ username: 'test' });
-			// console.log(test);
-			// await test.save();
-			// console.log(test.data._id);
+      // const test = userModel.instance({ username: 'test' });
+      // console.log(test);
+      // await test.save();
+      // console.log(test.data._id);
 
-			// const res = await userModel.deleteMany({});
+      // const res = await userModel.deleteMany({});
 
-			// console.log(res);
+      // console.log(res);
 
-			// ------------HOOKS---------------------
+      // ------------HOOKS---------------------
 
-			// userSchema.post('save', function () {
-			// 	console.log(this.data.username);
-			// });//Try to add types here as well
+      // userSchema.post('save', function () {
+      // 	console.log(this.data.username);
+      // });//Try to add types here as well
 
-			userSchema.pre('save', function() {
-				console.log('presaveHook');
-				this.data.username = 'test';
-				console.log(this);
-			});
-			const userModel = Model('User', userSchema);
+      // userSchema.pre('save', function () {
+      //   console.log('presaveHook');
+      //   this.data.username = 'test';
+      //   console.log(this);
+      // });
+      // const userModel = Model('User', userSchema);
 
-			const test = userModel.instance({ username: 'gkampitakisssss' });
+      // const test = userModel.instance({ username: 'gkampitakisssss' });
 
-			console.log(test.data.username);
+      // console.log(test.data.username);
 
-			const res = await test.save();
+      // const res = await test.save();
 
-			console.log(res);
+      // console.log(res);
 
-			//------------------------/
+      //------------------------/
 
-			//TODO: test the 
-			// const doc = Document('document_test', {});
-
-            // await doc.remove();
-            //TODO: investigate the usage of hooks and how you can get the data from all the hooks
-		} catch (error) {
-			console.log(error);
-		}
-	})
-	.catch(err => {
-		console.log(err);
-	});
+      //TODO: investigate the usage of hooks and how you can get the data from all the hooks
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
